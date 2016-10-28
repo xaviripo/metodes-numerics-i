8363441ab1b7355d2a5caab6d3514fbe0397397b
Xavier Ripoll <xaviripo97@gmail.com>
2016-10-28 14:56:13 +0200

    Finished Pràctica 1

--- Pràctica 1/exercici1.c
+++ Pràctica 1/exercici1.c
@@ -3,14 +3,14 @@
 /************ (i) ************/
 
 void power(int base, int lim) {
-	
+
 	int i, k = 1;
-	
+
 	for(i = 0; i <= lim; i++) {
-		printf("%i\n", k);
+		printf("%d\n", k);
 		k *= base;
 	}
-	
+
 }
 
 void i(void) {
@@ -26,35 +26,52 @@ void ii(void) {
 /************ (iii) ************/
 
 void iii(void) {
-	
+
 	int i;
-	
-	for(i = 0; i >= 0; i++) {
-	}
-	printf("%i\n", i-1);
-	
+
+	for(i = 0; i >= 0; i++);
+	printf("%d\n", i-1);
+
 }
 
 /************ (iv) ************/
 
+void iv(void) {
+
+	int i;
+
+	for(i = 0; i <= 0; i--) {
+	}
+	printf("%d\n", i+1);
+
+}
+
 /************ (v) ************/
+
+void v(void) {
+	int i, n = 1;
+	for(i = 1; i <= 35; i++) {
+		n *= i;
+		printf("%d\n", n);
+	}
+}
 
 /************ main ************/
 
-void exercici1(void) {
+int main(void) {
+
+	printf("Exercici 1\n\n");
 	printf("(i)\n");
 	i();
 	printf("\n(ii)\n");
 	ii();
 	printf("\n(iii)\n");
 	iii();
-}
+	printf("\n(iv)\n");
+	iv();
+	printf("\n(v)\n");
+	v();
 
-int main(void) {
-	
-	exercici1();
-	
 	return 0;
-	
-}
 
+}
---
+++ Pràctica 1/exercici3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+/************ main ************/
+
+int main(void) {
+
+	double x, e, epsilon = -1.0;
+	float a;
+
+	for(x = 0.01; x <= 1000.00; x += 0.01) {
+		a = x;
+		e = fabs((a-x)/x);
+		epsilon = fmax(e, epsilon);
+		printf("x: %07.2lf, a: %011f, e: %.100lf\n", x, a, e);
+	}
+	printf("epsilon: %.100lf\n", epsilon);
+	return 0;
+
+}
